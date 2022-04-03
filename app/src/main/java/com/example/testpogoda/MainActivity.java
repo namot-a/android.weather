a941176091db21bcae9d9e0f6b98528f1983f635
lalto <>
2022-04-03 17:22:32 +0800

    1

--- app/src/main/java/com/example/testpogoda/MainActivity.java
+++ app/src/main/java/com/example/testpogoda/MainActivity.java
@@ -1,11 +1,29 @@
 package com.example.testpogoda;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.loader.content.AsyncTaskLoader;
 
+import android.annotation.SuppressLint;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.Buffer;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,8 +38,87 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        user = findViewById()
+        user = findViewById(R.id.user);
+        main_btn = findViewById(R.id.main_btn);
+        result_info = findViewById(R.id.result_info);
+
+        main_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(user.getText().toString().trim().equals(""))
+                    Toast.makeText(MainActivity.this, R.string.no_user_input, Toast.LENGTH_LONG).show();
+                else {
+                    String city = user.getText().toString();
+                    String key = "01f95a1988cdcd96961eced343557851";
+                    String url = "https://api.openweathermap.org/data/2.5/weather?q="+ city +"&appid=" + key +"&units=metric&lang=ru";
+
+                    new GetURLDate().execute(url);
+
+                }
+            }
+        });
+
+    }
+
+    private class GetURLDate extends AsyncTask<String, String, String> {
 
 
+        protected void onPreExecute() {
+            super.onPreExecute();
+            result_info.setText("Ожидайте...");
+        }
+
+        @Override
+        protected String doInBackground(String... strings) {
+
+            HttpURLConnection connection = null;
+            BufferedReader reader = null;
+
+            try {
+                URL url = new URL(strings[0]);
+                connection = (HttpURLConnection) url.openConnection();
+                connection.connect();
+
+                InputStream stream = connection.getInputStream();
+                reader = new BufferedReader(new InputStreamReader(stream));
+
+                StringBuffer buffer = new StringBuffer();
+                String line = "";
+
+                while ((line = reader.readLine()) != null)
+                    buffer.append(line).append("\n");
+                return buffer.toString();
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                if (connection != null)
+                    connection.disconnect();
+
+                try {
+                    if (reader != null)
+                        reader.close();
+                }catch (IOException e){
+                    e.printStackTrace();
+                }
+            }
+            return null;
+        }
+
+        @SuppressLint("SetTextI18n")
+        @Override
+        protected void onPostExecute(String result){
+            super.onPostExecute(result);
+
+            try {
+                JSONObject jsonObject = new JSONObject(result);
+                result_info.setText("Температура:" + jsonObject.getJSONObject("main").getDouble("temp") + " ℃");
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+        }
     }
 }
