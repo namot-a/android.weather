afb5078930f188d5e8e132251298cdbc48f8a8b4
lalto <>
2022-04-03 15:33:05 +0800

    1

--- app/src/main/java/com/example/testpogoda/MainActivity.java
+++ app/src/main/java/com/example/testpogoda/MainActivity.java
@@ -3,12 +3,25 @@ package com.example.testpogoda;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private EditText user;
+    private Button main_btn;
+    private TextView result_info;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        user = findViewById()
+
+
     }
 }
